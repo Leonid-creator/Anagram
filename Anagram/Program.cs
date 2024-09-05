530b32978314a57fdd7615209f81b07f0fb27d15
DELL-LEONID <lenin0855@gmail.com>
2024-09-05 22:42:47 +0100

    Working program without tests

---
+++ Anagram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml;
+
+namespace Task1Anagram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter word or sentence: ");
+            string sentence = Console.ReadLine();
+
+            string reverseSentence = Anagram.Reverse(sentence);
+            Console.WriteLine("Reversed: " + reverseSentence);
+
+            Console.ReadLine();
+        }
+    }
+}
